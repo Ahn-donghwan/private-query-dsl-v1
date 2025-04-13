767cfefc2e4a7c7b5ac2264638d7b9b09928c8ae
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-04-13 16:48:46 +0900

    [common]chores: default settings
    - connect to Git

---
+++ src/main/java/com/unionclass/privatequerydslv1/PrivateQueryDslV1Application.java
@@ -0,0 +1,13 @@
+package com.unionclass.privatequerydslv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateQueryDslV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateQueryDslV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unionclass/privatequerydslv1/PrivateQueryDslV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionclass.privatequerydslv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateQueryDslV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
