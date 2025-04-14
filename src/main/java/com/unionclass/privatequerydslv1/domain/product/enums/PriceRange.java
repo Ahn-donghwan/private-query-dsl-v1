445679de34cda560f9325d2259d843e644a9e742
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-04-14 10:12:22 +0900

    [product]refactor: priceRange field name changed

--- src/main/java/com/unionclass/privatequerydslv1/domain/product/enums/PriceRange.java
+++ src/main/java/com/unionclass/privatequerydslv1/domain/product/enums/PriceRange.java
@@ -8,12 +8,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PriceRange {
 
-    BELOW_10K("1만원 미만", 0, 9999),
+    BELOW_10K("1만원미만", 0, 9999),
     TEN_K("1만원대", 10000, 19999),
     TWENTY_K("2만원대", 20000, 29999),
     THIRTY_K("3만원대", 30000, 39999),
     FORTY_K("4만원대", 40000, 49999),
-    ABOVE_50K("5만원 이상", 50000, 59999)
+    ABOVE_50K("5만원이상", 50000, 59999)
     ;
 
     private final String priceRange;
